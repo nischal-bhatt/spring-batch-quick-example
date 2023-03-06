db31a8c5e7834dd386bc0ef1b944f430256492f4
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-03-06 21:45:47 +0800

    asdadasdasasd

---
+++ src/main/java/com/javatechie/spring/batch/config/CustomerProcessor.java
@@ -0,0 +1,21 @@
+package com.javatechie.spring.batch.config;
+
+import com.javatechie.spring.batch.entity.Customer;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
+
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        if(customer.getCountry().equals("United States")) {
+        	customer.setId(ThreadLocalRandom.current().nextInt(0, 1000000));
+            return customer;
+        }else{
+            return null;
+        }
+    }
+}
